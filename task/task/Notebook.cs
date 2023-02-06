e142cba0f73150db7ef91ca083f1fe56bc21cd9b
Kenan <kananmh@code.edu.az>
2023-02-06 23:29:24 +0400

    task

---
+++ task/task/Notebook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Schema;
+
+namespace task
+{
+    internal class Notebook
+    {
+        public Notebook(string brand,string model)
+        {
+            this.Brand = brand;
+            this.Model = model;
+        }
+        public string Brand;
+        public string Model;
+        public int Price;
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Brand:{Brand} - model:{Model} - price:{Price}");
+        }
+    }
+}
---
+++ task/task/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook1 = new Notebook("Asus", "Tuf");
+            notebook1.Price = 1500;
+            notebook1.ShowInfo();
+
+            Notebook notebook2 = new Notebook("Hp","840 G8");
+            notebook2.Price = 1000;
+            notebook2.ShowInfo();
+
+            Notebook notebook3= new Notebook("Dell","Inspiron");
+            notebook3.Price = 1300;
+            notebook3.ShowInfo();
+
+            Notebook[]arr=new Notebook[] {notebook1,notebook2, notebook3};
+
+            var count = 0;
+            var sum =0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i].Price;
+                count++;
+                
+            }
+              Console.WriteLine($"ortalama qiymet:{ sum/count}");
+
+
+
+        }
+       
+    }
+}
